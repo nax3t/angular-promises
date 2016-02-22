37869eaa5811d6c40d09ed6e8a8729c1a01585d7
Ian Schoonover <iantskon@gmail.com>
2016-02-22 13:33:43 -0600

    Add http calls with promises using

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,10 @@
 app.controller('swCtrl', ['$scope', 'swService', function($scope, swService) {
-	$scope.films = '';
+	// put films array on scope and set it equal to the response from
+	// our getFilms method on the swService
+	// handle errors with a second callback argument to the .then function
+	$scope.films = swService.getFilms().then(function(skywalker) {
+		$scope.films = skywalker.films;
+	}, function (error) {
+		$scope.films = [{error: error.message}];
+	});
 }]);
--- js/services.js
+++ js/services.js
@@ -1,5 +1,42 @@
 app.service('swService', ['$http', '$q', function($http, $q) {
 	this.getFilms = function() {
+		// set base url for API call to a variable
+		// and create a promise using $q and set it to deferred variable
+		var baseUrl = 'http://swapi.co/api/',
+				deferred = $q.defer();
+		// make http GET request to base url + people/ and do something
+		// with the result
+		$http({
+			method: 'GET',
+			url: baseUrl + 'people/'
+		}).then(function(peopleData){
+			// set people to peopleData.data.results
+			// and set skywalker to first item in people
+			var people = peopleData.data.results,
+					skywalker = people[0];
+					skyWalkerFilms = [],
+					filmCount = 0;
+			// iterate over films array and make an http call for each
+			// film then assign its results to the original film item
+			// from the array
+			skywalker.films.forEach(function(film) {
+				$http.get(film)
+					.then(function(filmResult) {
+						skyWalkerFilms.push(filmResult.data);
+					})
+					.then(function() {
+						// when loop finishes, set skywalker.films to skyWalkerFilms
+						skywalker.films = skyWalkerFilms;
+						// resolve the promise with skywalker (this is what gets returned in the controller)
+						deferred.resolve(skywalker);
+					});
+			});
 
+		}, function (error) {
+			error.message = "Sorry, your request failed";
+			return deferred.reject(error);
+		});
+
+		return deferred.promise;
 	};
 }]);
