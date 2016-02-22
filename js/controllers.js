105956efed30bde71b7c4e076777971efaaee9cd
Ian Schoonover <iantskon@gmail.com>
2016-02-22 13:49:59 -0600

    Refactor controller code

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,7 @@
 app.controller('swCtrl', ['$scope', 'swService', function($scope, swService) {
-	// put films array on scope and set it equal to the response from
-	// our getFilms method on the swService
+	// call getFilms function from swService and set $scope.films to result
 	// handle errors with a second callback argument to the .then function
-	$scope.films = swService.getFilms().then(function(skywalker) {
+	swService.getFilms().then(function(skywalker) {
 		$scope.films = skywalker.films;
 	}, function (error) {
 		$scope.films = [{error: error.message}];
